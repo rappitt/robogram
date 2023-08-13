a41ee16b9f8304e156a9925292f38c765440a738
Nico <bua-nico@hotmail.de>
2023-08-13 11:44:42 +0200

    init push

---
+++ src/Components/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+	render() {
+		return ( 
+			<h1 className="text-3xl font-bold text-amber-300 text-center mt-10">
+				Hello world!
+			</h1>
+		);
+	}
+}
+ export default Hello;
